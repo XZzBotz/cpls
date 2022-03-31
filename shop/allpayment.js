4627efd90850d21b16e5ac600e3c6cfe0770d4bc
XZzBotz <ayemkecel@gmail.com>
2022-03-31 11:40:16 +0700

    cpls

---
+++ shop/allpayment.js
@@ -0,0 +1,35 @@
+const allpayment = (prefix) => {
+return `PAYMENT BY CUPLISSXZ
+
+*🏧 BANK*
+
+1. BRI 
+	ㅁ -
+2. BCA 
+	ㅁ -
+3. BNI 
+	ㅁ -
+4. PERMATA
+	ㅁ -
+	
+*💰 E-MONEY*
+
+1. GOPAY
+	ㅁ 0857 2697 0224
+2. OVO
+	ㅁ 0857 2697 0224
+3. DANA
+	ㅁ 0857 2697 0224
+	
+Semuanya atas nama CUPLISS
+
+*📲 PULSA*
+
+Tidak tersedia
+	
+Sebelum melakukan pembayaran ada baiknya anda menghubungi owner terlebih dahulu!
+`
+	}
+
+exports.allpayment = allpayment
+ 
---
+++ shop/donasi.js
@@ -0,0 +1,29 @@
+const donasibot = () => {
+return `DONASI BISA VIA BANK / E-MONEY
+
+*🏧 BANK*
+
+1. BRI 
+	ㅁ -
+2. BCA 
+	ㅁ -
+3. BNI 
+	ㅁ -
+4. PERMATA
+	ㅁ -
+	
+*💰 E-MONEY*
+
+1. GOPAY
+	ㅁ 0887 4350 47326
+2. OVO
+	ㅁ 0887 4350 47326
+3. DANA
+	ㅁ 0887 4350 47326
+	
+Semuanya atas nama CuplissXz
+`
+	}
+
+exports.donasibot = donasibot
+ 
